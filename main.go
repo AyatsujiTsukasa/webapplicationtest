10fa720c624dc73c7a177314dd4d9d78213998f2
Takumi Ito <takumi10194617@gmail.com>
2017-09-01 03:06:29 +0900

    Initial commit

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"flag"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"sync"
+
+	"github.com/AyatsujiTsukasa/Trace"
+)
+
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+	t.templ.Execute(w, r)
+}
+
+func main() {
+	var addr = flag.String("addr", ":8080", "application address")
+	flag.Parse()
+
+	r := newRoom()
+	r.tracer = trace.New(os.Stdout)
+
+	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/room", r)
+
+	go r.run()
+
+	log.Println("Web server start running. port", *addr)
+	if err := http.ListenAndServe(*addr, nil); err != nil {
+		log.Fatal(err.Error())
+	}
+}
